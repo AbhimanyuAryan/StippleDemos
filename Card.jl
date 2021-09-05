018b9adfe50415b65bf99b40e5ec2f5ffb3166b5
Abhimanyu Aryan <8083613+AbhimanyuAryan@users.noreply.github.com>
2021-09-05 16:28:56 +0530

    q__card example with inline styling

---
+++ Card.jl
@@ -0,0 +1,41 @@
+# This exmaple show how to add use q-card with inline styling 
+
+using Stipple
+using StippleUI
+
+Base.@kwdef mutable struct CardDemo <: ReactiveModel
+
+end
+
+hs_model = Stipple.init(CardDemo())
+
+function ui()
+    [
+        page(
+            vm(hs_model), class="container", title="Card Demo", partial=true,
+            [
+                row(
+                    cell([
+                        h1("Knob example")
+                    ])
+                )
+                row(
+                    cell([
+                        card(class="text-white", style="background: radial-gradient(circle, #35a2ff 0%, #014a88 100%); width: 30%",
+                        card_section("lorLorem Ipsum is simply dummy text of the printing 
+                        and typesetting industry. Lorem Ipsum has been the industry's standard
+                         dummy text ever since the 1500s, when an unknown printer took a galley 
+                         of type and scrambled it to make a type specimen book. It has survived 
+                         not only five centuries, but also the leap into electronic typesetting,
+                          remaining essentially unchanged. It was popularised in the 1960s with 
+                          the release of Letraset sheets containing Lorem Ipsum passages, and more
+                           recently with desktop publishing software like Aldus PageMaker including 
+                           versions of Lorem Ipsumem"))
+                    ])
+                )
+            ]
+        )
+    ]
+end
+
+route("/", ui)
